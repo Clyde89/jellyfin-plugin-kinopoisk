9179bb53fa20dd182f5824084c411b16f6e5307c
Андрей <civilika@yandex.ru>
2026-07-31 21:26:08 +0500

    Восстановлена совместимость конструктора сериалов

--- src/Jellyfin.Plugin.Kinopoisk/MetadataProviders/SeriesMetadataProvider.cs
+++ src/Jellyfin.Plugin.Kinopoisk/MetadataProviders/SeriesMetadataProvider.cs
@@ -1,5 +1,6 @@
 using System.Net.Http;
 using Jellyfin.Plugin.Kinopoisk.ProviderIdResolvers;
+using Jellyfin.Plugin.Kinopoisk.Services;
 using KinopoiskUnofficialInfo.ApiClient;
 using MediaBrowser.Controller.Entities.TV;
 using MediaBrowser.Controller.Providers;
@@ -9,6 +10,21 @@ namespace Jellyfin.Plugin.Kinopoisk.MetadataProviders
 {
     public class SeriesMetadataProvider : BaseVideoMetadataProvider<Series, SeriesInfo>
     {
+        public SeriesMetadataProvider(
+            IKinopoiskApiClient kinopoiskApiClient,
+            IProviderIdResolver<SeriesInfo> providerIdResolver,
+            ILogger<SeriesMetadataProvider> logger,
+            IHttpClientFactory httpClientFactory)
+            : this(
+                kinopoiskApiClient,
+                kinopoiskApiClient as IKinopoiskDistributionApiClient
+                    ?? EmptyKinopoiskDistributionApiClient.Instance,
+                providerIdResolver,
+                logger,
+                httpClientFactory)
+        {
+        }
+
         public SeriesMetadataProvider(
             IKinopoiskApiClient kinopoiskApiClient,
             IKinopoiskDistributionApiClient distributionApiClient,
