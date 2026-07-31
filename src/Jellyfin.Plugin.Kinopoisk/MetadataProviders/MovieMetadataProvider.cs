1cdae800f45103b0d7630079b3f1e5695b672834
Андрей <civilika@yandex.ru>
2026-07-31 21:25:50 +0500

    Восстановлена совместимость конструктора фильмов

--- src/Jellyfin.Plugin.Kinopoisk/MetadataProviders/MovieMetadataProvider.cs
+++ src/Jellyfin.Plugin.Kinopoisk/MetadataProviders/MovieMetadataProvider.cs
@@ -1,5 +1,6 @@
 using System.Net.Http;
 using Jellyfin.Plugin.Kinopoisk.ProviderIdResolvers;
+using Jellyfin.Plugin.Kinopoisk.Services;
 using KinopoiskUnofficialInfo.ApiClient;
 using MediaBrowser.Controller.Entities.Movies;
 using MediaBrowser.Controller.Providers;
@@ -9,6 +10,21 @@ namespace Jellyfin.Plugin.Kinopoisk.MetadataProviders
 {
     public class MovieMetadataProvider : BaseVideoMetadataProvider<Movie, MovieInfo>
     {
+        public MovieMetadataProvider(
+            IKinopoiskApiClient kinopoiskApiClient,
+            IProviderIdResolver<MovieInfo> providerIdResolver,
+            ILogger<MovieMetadataProvider> logger,
+            IHttpClientFactory httpClientFactory)
+            : this(
+                kinopoiskApiClient,
+                kinopoiskApiClient as IKinopoiskDistributionApiClient
+                    ?? EmptyKinopoiskDistributionApiClient.Instance,
+                providerIdResolver,
+                logger,
+                httpClientFactory)
+        {
+        }
+
         public MovieMetadataProvider(
             IKinopoiskApiClient kinopoiskApiClient,
             IKinopoiskDistributionApiClient distributionApiClient,
