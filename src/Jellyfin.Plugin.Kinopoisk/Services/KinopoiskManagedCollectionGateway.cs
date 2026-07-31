c95a495a37fd15fdf7a582bd86ef305af0dd6b1a
Андрей <civilika@yandex.ru>
2026-07-31 23:50:55 +0500

    Исправлено пространство имён запроса медиатеки

--- src/Jellyfin.Plugin.Kinopoisk/Services/KinopoiskManagedCollectionGateway.cs
+++ src/Jellyfin.Plugin.Kinopoisk/Services/KinopoiskManagedCollectionGateway.cs
@@ -6,6 +6,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Jellyfin.Data.Enums;
 using MediaBrowser.Controller.Collections;
+using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Entities.Movies;
 using MediaBrowser.Controller.Library;
 
