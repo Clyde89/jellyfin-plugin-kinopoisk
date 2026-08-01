e8ee4ce0d15eb24efa014143904145adfea9806f
Андрей <civilika@yandex.ru>
2026-08-01 09:13:49 +0500

    Добавлена совместимость имени диагностического уровня

--- src/Jellyfin.Plugin.Kinopoisk/Configuration/DiagnosticLogLevel.cs
+++ src/Jellyfin.Plugin.Kinopoisk/Configuration/DiagnosticLogLevel.cs
@@ -6,4 +6,13 @@ namespace Jellyfin.Plugin.Kinopoisk.Configuration
         Detailed = 1,
         Trace = 2
     }
+
+    public static class DiagnosticLogLevel
+    {
+        public const KinopoiskDiagnosticLevel Basic = KinopoiskDiagnosticLevel.Basic;
+
+        public const KinopoiskDiagnosticLevel Detailed = KinopoiskDiagnosticLevel.Detailed;
+
+        public const KinopoiskDiagnosticLevel Trace = KinopoiskDiagnosticLevel.Trace;
+    }
 }
