8d3a8c168ca2cce5c44701500f99ecde16c997ee
Андрей <civilika@yandex.ru>
2026-08-04 14:44:48 +0500

    Добавлены тесты нормализации внешнего текста

---
+++ src/Jellyfin.Plugin.Kinopoisk.Tests/KinopoiskPresentationTextSanitizerTests.cs
@@ -0,0 +1,55 @@
+using Jellyfin.Plugin.Kinopoisk.Presentation;
+using Xunit;
+
+namespace Jellyfin.Plugin.Kinopoisk.Tests
+{
+    public class KinopoiskPresentationTextSanitizerTests
+    {
+        [Fact]
+        public void ShouldPreserveAnchorTextAndRemoveMarkup()
+        {
+            const string source =
+                "На роль героини взяли <a href=\"/name/1/\">одну актрису</a>, "
+                + "но позднее её сменила <a href=\"/name/2/\">другая актриса</a>.";
+
+            var result = KinopoiskPresentationTextSanitizer.NormalizePlainText(source);
+
+            Assert.Equal(
+                "На роль героини взяли одну актрису, но позднее её сменила другая актриса.",
+                result);
+        }
+
+        [Fact]
+        public void ShouldDecodeEntitiesAndPreserveParagraphBoundaries()
+        {
+            const string source = "<p>Первый&nbsp;абзац.</p><p>Второй &amp; третий.</p>";
+
+            var result = KinopoiskPresentationTextSanitizer.NormalizePlainText(source);
+
+            Assert.Equal("Первый абзац.\n\nВторой & третий.", result);
+        }
+
+        [Fact]
+        public void ShouldRemoveExecutableMarkup()
+        {
+            const string source =
+                "Текст<script>alert('x')</script><img src=x onerror=alert(1)> продолжен.";
+
+            var result = KinopoiskPresentationTextSanitizer.NormalizePlainText(source);
+
+            Assert.DoesNotContain("<", result);
+            Assert.DoesNotContain(">", result);
+            Assert.DoesNotContain("onerror", result);
+            Assert.Equal("Текстalert('x') продолжен.", result);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public void ShouldReturnEmptyTextForEmptyInput(string? source)
+        {
+            Assert.Equal(string.Empty, KinopoiskPresentationTextSanitizer.NormalizePlainText(source));
+        }
+    }
+}
