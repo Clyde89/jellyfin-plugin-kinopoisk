a8cc29b6ef7e74a6fada72ae899e3b76dd746368
Андрей <civilika@yandex.ru>
2026-08-05 19:52:43 +0500

    Уточнено состояние автономного веб-клиента

--- src/Jellyfin.Plugin.Kinopoisk/Services/KinopoiskWebTrailerIntegrationState.cs
+++ src/Jellyfin.Plugin.Kinopoisk/Services/KinopoiskWebTrailerIntegrationState.cs
@@ -3,20 +3,20 @@ using System.Threading;
 namespace Jellyfin.Plugin.Kinopoisk.Services
 {
     /// <summary>
-    /// Хранит состояние клиентской интеграции трейлеров КиноПоиска.
+    /// Хранит состояние автономной клиентской интеграции КиноПоиска.
     /// </summary>
     public static class KinopoiskWebTrailerIntegrationState
     {
         private static int _isRegistered;
 
         /// <summary>
-        /// Получает признак успешной регистрации веб-плеера через JavaScript Injector.
+        /// Получает признак успешной установки и проверки автономного веб-клиента.
         /// </summary>
         public static bool IsRegistered
             => Volatile.Read(ref _isRegistered) == 1;
 
         /// <summary>
-        /// Обновляет состояние регистрации веб-плеера.
+        /// Обновляет состояние автономного веб-клиента.
         /// </summary>
         /// <param name="isRegistered">Новое состояние.</param>
         public static void SetRegistered(bool isRegistered)
