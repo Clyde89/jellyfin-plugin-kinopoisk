0ef9b5af2704b733e6fd78fd10ec37a14a1a0dee
Андрей <civilika@yandex.ru>
2026-08-05 20:26:51 +0500

    Обновлён тест моста Elsewhere в автономном bundle

--- src/Jellyfin.Plugin.Kinopoisk.Tests/KinopoiskElsewhereBridgeTests.cs
+++ src/Jellyfin.Plugin.Kinopoisk.Tests/KinopoiskElsewhereBridgeTests.cs
@@ -36,7 +36,7 @@ namespace Jellyfin.Plugin.Kinopoisk.Tests
         }
 
         [Fact]
-        public void ShouldRegisterElsewhereBridgeInStandaloneWebClient()
+        public void ShouldRegisterElsewhereBridgeInAutonomousBundle()
         {
             var projectDirectory = Path.Combine(
                 AppContext.BaseDirectory,
@@ -47,16 +47,16 @@ namespace Jellyfin.Plugin.Kinopoisk.Tests
                 "Jellyfin.Plugin.Kinopoisk");
             var registrator = File.ReadAllText(
                 Path.Combine(projectDirectory, "KinopoiskPluginServiceRegistrator.cs"));
-            var webClient = File.ReadAllText(
+            var bundle = File.ReadAllText(
                 Path.Combine(
                     projectDirectory,
                     "Services",
-                    "KinopoiskStandaloneWebClientService.cs"));
+                    "KinopoiskWebClientBundle.cs"));
 
             Assert.Contains(
                 "AddHostedService<KinopoiskStandaloneWebClientService>()",
                 registrator);
-            Assert.Contains("kinopoiskElsewhereBridge.js", webClient);
+            Assert.Contains("kinopoiskElsewhereBridge.js", bundle);
         }
     }
 }
