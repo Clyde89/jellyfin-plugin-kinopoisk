b297bfc61c2ad9e4dbdd76188d2a460157651634
Андрей <civilika@yandex.ru>
2026-08-05 19:46:25 +0500

    Обновлён тест порядка исправлений интерфейса

--- src/Jellyfin.Plugin.Kinopoisk.Tests/KinopoiskRuntimeUiCorrectionsTests.cs
+++ src/Jellyfin.Plugin.Kinopoisk.Tests/KinopoiskRuntimeUiCorrectionsTests.cs
@@ -46,7 +46,7 @@ namespace Jellyfin.Plugin.Kinopoisk.Tests
                     "..",
                     "Jellyfin.Plugin.Kinopoisk",
                     "Services",
-                    "KinopoiskWebPresentationIntegrationService.cs"));
+                    "KinopoiskStandaloneWebClientService.cs"));
 
             var polishIndex = source.IndexOf(
                 "kinopoiskRuntimePolish.js",
