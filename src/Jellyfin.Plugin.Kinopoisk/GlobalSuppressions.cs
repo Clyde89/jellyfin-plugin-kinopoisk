460b55aef09ac2fa47fcaf350caa9edf4350e79d
Андрей <civilika@yandex.ru>
2026-08-12 11:24:02 +0500

    Очищена ветка стабильного каталога

--- src/Jellyfin.Plugin.Kinopoisk/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage(
-    "Interoperability",
-    "CA1416:Validate platform compatibility",
-    Justification = "Unix-права восстанавливаются только после успешной проверки OperatingSystem.IsWindows; nullable-состояние previousMode сохраняет результат этой проверки между операциями атомарной записи.",
-    Scope = "member",
-    Target = "~M:Jellyfin.Plugin.Kinopoisk.Services.KinopoiskStandaloneWebClientService.WriteTextAtomically(System.String,System.String)")]
